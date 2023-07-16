87ccff8e8d9fcd79e9e4791747fc567a8a6554f4
klarmee <126979369+klarmee@users.noreply.github.com>
2023-07-16 19:25:24 +0000

    update

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,41 +1,106 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, Fragment } from 'react';
 export default function Textarea() {
-  const [state, setState] = useState([null])
+  const timeRef = useRef(undefined);
   const counter = useRef(0);
   const selection = useRef('');
+  const [state, setState] = useState([{ id: null, html: null }])
+
   useEffect(() => {
-    var storage = window.localStorage.getItem("storage") ? window.localStorage.getItem("storage") : null
-    if (storage) document.documentElement.innerHTML = storage
-    window.addEventListener('load', initFocus)
+    window.addEventListener('load', init)
+    window.addEventListener('beforeunload', confirmExit);
     document.addEventListener('selectionchange', handleSelection);
-    document.addEventListener('keydown', handleKeydown);
-    document.addEventListener('keyup', handleKeyup);
-    function handleKeydown(e) {
-      if (e.key === 'Backspace' && document.activeElement.innerText == '' && document.activeElement.id) {
-        var id = document.activeElement.id
-        placeCaretAtEnd(document.activeElement.previousSibling)
-        if (document.querySelector(`a.${id}`)) document.querySelector(`a.${id}`).replaceWith(id)
-        let newState = state.filter(function (item) {
-          return item !== id
-        })
-        setState(newState)
-      }
-    }
-    function handleKeyup() {
-      window.localStorage.setItem("storage", document.documentElement.innerHTML)
-      console.log('saved');
-    }
     return () => {
-      window.removeEventListener('load', initFocus)
+      window.removeEventListener('load', init)
+      window.removeEventListener('beforeunload', confirmExit);
       document.removeEventListener('selectionchange', handleSelection);
-      document.removeEventListener('keydown', handleKeydown);
-      document.removeEventListener('keyup', handleKeyup);
     }
-  })
-  function handleSelection() {
-    const currentSelection = document.getSelection()
-    if (currentSelection) {
-      selection.current = currentSelection;
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    }
+  }, [state])
+
+  return (
+    <>
+      {
+        state.map((link, i) => {
+          if (link.html) return (
+            <div dangerouslySetInnerHTML={{ __html: link.html }} id={link.id} data-index={i} key={i} spellCheck='false' className={'div ' + link.id} contentEditable='true' tabIndex='1' suppressContentEditableWarning={true} />
+          )
+          else return (
+            <div id={link.id} data-index={i} key={i} spellCheck='false' className={'div ' + link.id} contentEditable='true' tabIndex='1' suppressContentEditableWarning={true}>
+              {link.id}
+            </div>
+          )
+        })
+      }
+    </>
+  )
+
+  function handleKeydown(e) {
+    link(e)
+    unLink(e)
+    saveToStorage()
+  }
+  function init() {
+    loadFromStorage()
+    initFocus()
+  }
+  function link(e) {
+    if (e.ctrlKey && e.key === 'l') {
+      var newId = selection.current.toString()
+      let idExists = state.find(link => link.id == newId)
+      console.log(idExists)
+      if (idExists == undefined && newId == encodeURIComponent(newId)) {
+        replaceTextWithLink(newId)
+        setState([...state, { id: newId }])
+        addStyle(newId)
+        e.preventDefault()
+      }
+      window.localStorage.setItem(newId + '-style', document.querySelector('style.' + newId).innerHTML)
+    }
+  }
+  function unLink(e) {
+    if (e.key === 'Backspace' && document.activeElement.innerText == '' && document.activeElement.id) {
+      var id = document.activeElement.id
+      placeCaretAtEnd(document.activeElement.previousSibling)
+      if (document.querySelector(`a.${id}`)) document.querySelector(`a.${id}`).replaceWith(id)
+      let newState = state.filter(link => link.id !== id)
+      setState(newState)
+      window.localStorage.removeItem(id + '-style')
+    }
+  }
+  function saveToStorage() {
+    if (timeRef.current !== undefined) {
+      clearTimeout(timeRef.current)
+      timeRef.current = undefined
+    }
+    timeRef.current = setTimeout(
+      () => {
+        let newState = []
+        Array.from(document.querySelectorAll('.div')).forEach((div) => {
+          newState.push({ id: div.id, html: div.innerHTML })
+        })
+        window.localStorage.setItem("state", JSON.stringify(newState))
+      },
+      200
+    )
+  };
+
+  function loadFromStorage() {
+    if (window.localStorage.getItem("state")) {
+      setState(JSON.parse(window.localStorage.getItem("state")))
+      JSON.parse(window.localStorage.getItem("state")).map(link => {
+        if (window.localStorage.getItem(link.id + "-style")) {
+          console.log(window.localStorage.getItem(link.id + "-style"));
+          let style = document.createElement('style')
+          style.innerHTML = window.localStorage.getItem(link.id + "-style")
+          document.head.append(style)
+        }
+      })
     }
   }
   function initFocus() {
@@ -44,24 +109,23 @@ export default function Textarea() {
     }, 0);
   }
 
-  return (
-    <>
-      {state.map((v, i) => <div onKeyDown={(e) => link(e, i)} id={v} data-index={i} key={i} spellCheck='false' className={'div ' + v} contentEditable='true' tabIndex='1'>{v}</div>)}
-    </>
-  )
-
-  function link(e) {
-    let id = selection.current.toString()
-    if (e.ctrlKey && e.key === 'l' && state.includes(id) == false && id == encodeURIComponent(id)) {
-      replaceTextWithLink(id)
-      setState([...state, id])
-      addStyle(id)
-      e.preventDefault()
+  function confirmExit() {
+    return "exit?";
+  }
+  function handleSelection() {
+    const currentSelection = document.getSelection()
+    if (currentSelection) {
+      selection.current = currentSelection;
     }
   }
 
+
+
+
+
   function addStyle(className) {
     var styleSheet = document.createElement("style")
+    styleSheet.className = className
     var hue = `hsl(${counter.current}, 50%, 50%)`
     styleSheet.innerText = `a.${className}{border-bottom:solid ${hue};}div.${className}{border-left:solid ${hue};}`
     document.head.appendChild(styleSheet)
